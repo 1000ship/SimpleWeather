f2bfc4d3c369ec7c71862918d9e4056a75df63f9
1000ship <cjstjdgur1234@gmail.com>
2020-01-14 20:23:21 +0900

    Initial Commit

---
+++ SimpleWeather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SimpleWeather
+//
+//  Created by 천성혁 on 2020/01/14.
+//  Copyright © 2020 1000ship. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
