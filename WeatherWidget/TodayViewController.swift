8708b901e69f69bdfd3ba13c5037f29cee64f719
1000ship <cjstjdgur1234@gmail.com>
2020-01-16 11:30:07 +0900

    Fix bug

--- WeatherWidget/TodayViewController.swift
+++ WeatherWidget/TodayViewController.swift
@@ -12,12 +12,15 @@ import CoreFoundation
 
 class TodayViewController: UIViewController, NCWidgetProviding {
 
-//    @IBOutlet weak var temp : UITextView!
+    
+    @IBOutlet weak var tempText: UILabel!
+    @IBOutlet weak var windText: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-
+        tempText.text = ""
+        windText.text = ""
     }
     
     private func getBeside( str : String, from : String, to : String, index : Int = 0 ) -> String
@@ -39,10 +42,12 @@ class TodayViewController: UIViewController, NCWidgetProviding {
             let data = try Data(contentsOf: url)
             let html = String(data: data, encoding: String.Encoding(rawValue: CFStringConvertEncodingToNSStringEncoding( 0x0422 )))!
             let partial = getBeside(str: html, from: "<div class=\"local_forecast_inn\">", to: "_<>_<>_")
-//            temp = getBeside(str: partial, from: "<dd class=\"now_weather1_center temp1 MB10\">", to: "</dd>")
-//            wind = getBeside(str: partial, from: "<dd class=\"now_weather1_center\">", to: "</dd>")
+            let temp = getBeside(str: partial, from: "<dd class=\"now_weather1_center temp1 MB10\">", to: "</dd>")
+            let wind = getBeside(str: partial, from: "<dd class=\"now_weather1_center\">", to: "</dd>")
 //            humi = getBeside(str: partial, from: "<dd class=\"now_weather1_center\">", to: "</dd>", index: 1)
-            print(1)
+            
+            tempText.text = temp
+            windText.text = wind
         }
         catch
         {
